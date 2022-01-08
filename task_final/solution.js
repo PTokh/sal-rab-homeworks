1ea9939ac878be78bad1beee35939a767bc8560a
Polina Tokh <malinat403@mail.ru>
2022-01-08 23:20:15 +0300

    final commit

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,12 +34,13 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {client: {name,phone}, order: {address, sum}, goods: [{title: "Пицца", count: 2} ]};
+    let data = {client: {name,phone}, order: {address, sum}, goods: []};
 
     let countOfGoods = goods.length;
 
     for (let i = 0; i < countOfGoods; i ++) {
-        data.goods.push(goods[i].title);
+        data.goods.push({"title": goods[i].title, "count": goods[i].count});
+    
     }
 
     data.order.address = "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, кв " + address.flat;
